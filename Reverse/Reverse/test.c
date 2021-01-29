3b04b2f3aedc2d18e688841c7a9fca6c40a8a556
chenlong_cxy <chenlonghyl@163.com>
2021-01-29 14:20:49 +0800

    逆序输出字符串(含空格)

--- Reverse/Reverse/test.c
+++ Reverse/Reverse/test.c
@@ -48,23 +48,49 @@
 //}
 
 //·¨Èý
+//#include<stdio.h>
+//#include<string.h>
+//void Reverse(char* str)
+//{
+//	int len = strlen(str);
+//	char temp = *str;
+//	*str = *(str + len - 1);
+//	*(str + len - 1) = '\0';
+//	if (strlen(str + 1) >= 2)
+//	{
+//		Reverse(str + 1);
+//	}
+//	*(str + len - 1) = temp;
+//}//µÝ¹é
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	Reverse(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
 #include<stdio.h>
 #include<string.h>
 void Reverse(char* str)
 {
 	int len = strlen(str);
-	char temp = *str;
-	*str = *(str + len - 1);
-	*(str + len - 1) = '\0';
-	if (strlen(str + 1) >= 2)
+	char* left = str;
+	char* right = str + len - 1;
+	while (left<right)
 	{
-		Reverse(str + 1);
+		int temp = *(left);
+		*(left) = *(right);
+		*(right) = temp;
+		left++;
+		right--;
 	}
-	*(str + len - 1) = temp;
-}//µÝ¹é
+}
 int main()
 {
-	char arr[] = "abcdef";
+	char arr[100];
+	gets(arr);
 	Reverse(arr);
 	printf("%s\n", arr);
 	return 0;
